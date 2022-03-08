d05b265ad4c655bebcd555a98f531835958ee57f
Kevin Karnani <kevinkarnani@gmail.com>
2022-03-07 21:26:41 -0500

    edge_weight issue

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from types import new_class
 from unicodedata import decimal
 from dataset import Dataset
-#nfrom filtration import FilterManager, FilterBlackAndWhite, FilterHSV
+# from filtration import FilterManager, FilterBlackAndWhite, FilterHSV
 from model_manager import ModelManager
 from tqdm import tqdm
 import numpy as np
@@ -30,7 +30,6 @@ parser.add_argument('--patch-size', type=int, default=224)
 parser.add_argument('--train-labels', type=str, default='train_labels.csv')
 parser.add_argument('--test-labels', type=str, default='test_labels.csv')
 parser.add_argument('--num-workers', type=int, default=0)
-parser.add_argument('--edge-weight', type=float, default=1.0)
 
 args = vars(parser.parse_args())
 
@@ -38,7 +37,8 @@ dataname = "digpath_supervised"
 SM_CHANNEL_TRAIN = os.getenv('SM_CHANNEL_TRAIN')
 SM_CHANNEL_TEST = os.getenv('SM_CHANNEL_TEST')
 SM_OUTPUT_DIR = os.getenv('SM_OUTPUT_DIR')
-num_classes = args['num_classes']  # number of classes in the data mask that we'll aim to predict
+# number of classes in the data mask that we'll aim to predict
+num_classes = args['num_classes']
 in_channels = args['in_channels']  # input channel of the data, RGB = 3
 growth_rate = args['growth_rate']
 block_config = args['block_config']
@@ -46,12 +46,12 @@ num_init_features = args['num_init_features']
 bn_size = args['bn_size']
 drop_rate = args['drop_rate']
 batch_size = args['batch_size']
-patch_size = args['patch_size']  # currently, this needs to be 224 due to densenet architecture
+# currently, this needs to be 224 due to densenet architecture
+patch_size = args['patch_size']
 num_epochs = args['num_epochs']
 train_labels = f'{os.getcwd()}/{args["train_labels"]}'
 test_labels = f'{os.getcwd()}/{args["test_labels"]}'
 num_workers = args['num_workers']
-edge_weight = args['edge_weight']
 phases = ["train", 'val']  # how many phases did we create databases for?
 # when should we do valiation? note that validation is *very* time consuming, so as opposed to doing for both training and validation, we do it only for vlaidation at the end of the epoch
 validation_phases = ['val']
@@ -71,51 +71,62 @@ class MyModel:
         self.model.train()
         for ii, (X, label) in enumerate(data_loader):
             X = X.to(self.device)
-            label = torch.tensor(list(map(lambda x: int(x), label))).to(self.device)
+            label = torch.tensor(
+                list(map(lambda x: int(x), label))).to(self.device)
             with torch.set_grad_enabled(True):
-                prediction = self.model(X.permute(0, 3, 1, 2).float())  # [N, Nclass]
+                prediction = self.model(
+                    X.permute(0, 3, 1, 2).float())  # [N, Nclass]
                 loss = self.loss_fn(prediction, label)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
-                self.all_loss['train'] = torch.cat((self.all_loss['train'], loss.detach().view(1, -1)))
-        self.all_acc['train'] = (self.cmatrix['train'] / self.cmatrix['train'].sum()).trace()
+                self.all_loss['train'] = torch.cat(
+                    (self.all_loss['train'], loss.detach().view(1, -1)))
+        self.all_acc['train'] = (
+            self.cmatrix['train'] / self.cmatrix['train'].sum()).trace()
         self.all_loss['train'] = self.all_loss['train'].cpu().numpy().mean()
-    
+
     def eval(self, data_loader):
         self.model.eval()
         for ii, (X, label) in enumerate(data_loader):
             X = X.to(self.device)
-            label = torch.tensor(list(map(lambda x: int(x), label))).to(self.device)
+            label = torch.tensor(
+                list(map(lambda x: int(x), label))).to(self.device)
             with torch.no_grad():
-                prediction = self.model(X.permute(0, 3, 1, 2).float())  # [N, Nclass]
+                prediction = self.model(
+                    X.permute(0, 3, 1, 2).float())  # [N, Nclass]
                 loss = self.loss_fn(prediction, label)
                 p = prediction.detach().cpu().numpy()
                 cpredflat = np.argmax(p, axis=1).flatten()
                 yflat = label.cpu().numpy().flatten()
 
-                self.all_loss['val'] = torch.cat((self.all_loss['val'], loss.detach().view(1, -1)))
-                self.cmatrix['val'] = self.cmatrix['val'] + confusion_matrix(yflat, cpredflat, labels=range(num_classes))
-        self.all_acc['val'] = (self.cmatrix['val'] / self.cmatrix['val'].sum()).trace()
+                self.all_loss['val'] = torch.cat(
+                    (self.all_loss['val'], loss.detach().view(1, -1)))
+                self.cmatrix['val'] = self.cmatrix['val'] + \
+                    confusion_matrix(yflat, cpredflat,
+                                     labels=range(num_classes))
+        self.all_acc['val'] = (self.cmatrix['val'] /
+                               self.cmatrix['val'].sum()).trace()
         self.all_loss['val'] = self.all_loss['val'].cpu().numpy().mean()
-    
+
     def diagnose(self, region_stream):
-        votes = {0:0, 1:0, 2:0}
+        votes = {0: 0, 1: 0, 2: 0}
         key = {0: 'MILD', 1: 'Moderate', 2: 'Severe'}
         for region in region_stream:
             self.model.eval()
             output = self.model(region[None, ::].to(self.device))
             output = output.detach().squeeze().cpu().numpy()
             votes[np.argmax(output)] += 1
-        return key[max(votes, key=votes.get)] # key with max value
+        return key[max(votes, key=votes.get)]  # key with max value
 
 
 def main():
     train_dir = SM_CHANNEL_TRAIN
     test_dir = SM_CHANNEL_TEST
     output_dir = SM_OUTPUT_DIR
-    filtration = None # FilterManager(filters=[FilterBlackAndWhite(), FilterHSV()])
+    # FilterManager(filters=[FilterBlackAndWhite(), FilterHSV()])
+    filtration = None
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(device)
     model = DenseNet(growth_rate=growth_rate, block_config=block_config,
@@ -129,16 +140,17 @@ def main():
     dataset['train'] = Dataset(
         data_dir=train_dir, labels=train_labels, filtration=filtration)
     dataLoader['train'] = DataLoader(dataset['train'], batch_size=batch_size,
-                                    shuffle=True, num_workers=num_workers, pin_memory=True)
+                                     shuffle=True, num_workers=num_workers, pin_memory=True)
     print(f"train dataset size:\t{len(dataset['train'])}")
     dataset['val'] = Dataset(
         data_dir=test_dir, labels=test_labels, filtration=filtration)
     dataLoader['val'] = DataLoader(dataset['val'], batch_size=batch_size,
-                                    shuffle=True, num_workers=num_workers, pin_memory=True)
+                                   shuffle=True, num_workers=num_workers, pin_memory=True)
     print(f"val dataset size:\t{len(dataset['val'])}")
     criterion = nn.CrossEntropyLoss()
 
     best_loss_on_test = np.Infinity
+    edge_weight = 1.0
     edge_weight = torch.tensor(edge_weight).to(device)
     manager = ModelManager(output_dir)
     for epoch in tqdm(range(num_epochs)):
@@ -148,7 +160,8 @@ def main():
         all_loss = {key: torch.zeros(0).to(device) for key in phases}
         cmatrix = {key: np.zeros((num_classes, num_classes)) for key in phases}
 
-        my_model = MyModel(model, criterion, device, all_acc, all_loss, cmatrix)
+        my_model = MyModel(model, criterion, device,
+                           all_acc, all_loss, cmatrix)
         my_model.train_model(optim, dataLoader['train'])
         my_model.eval(dataLoader['val'])
 
@@ -172,7 +185,8 @@ def main():
                      'num_classes': num_classes}
 
             # torch.save(state, f"{dataname}_densenet_best_model.pth")
-            manager.save_model(model_name=f"{dataname}_densenet_best_model", model=model, model_info=state, overwrite_model=True)
+            manager.save_model(model_name=f"{dataname}_densenet_best_model",
+                               model=model, model_info=state, overwrite_model=True)
         else:
             print("")
     diagnose_example(model, manager, dataset, device)
@@ -181,7 +195,8 @@ def main():
 def diagnose_example(model, manager, dataset, device):
     img, label, _ = dataset["val"][2]
     manager.load_model(f"{dataname}_densenet_best_model")
-    model.load_state_dict(manager.get_model_info(f"{dataname}_densenet_best_model")['model_dict'])
+    model.load_state_dict(manager.get_model_info(
+        f"{dataname}_densenet_best_model")['model_dict'])
     output = model(img[None, ::].to(device))
     output = output.detach().squeeze().cpu().numpy()
     print(f"True class:{label}")
