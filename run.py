76ec78b3517945fbb4f7e698344d9bb37fc17964
Kevin Karnani <kevinkarnani@gmail.com>
2022-05-16 17:44:34 +0000

    added tiles and augmentation again

--- run.py
+++ run.py
@@ -14,9 +14,11 @@ from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader, WeightedRandomSampler
 from torchvision.models import DenseNet
+from torchvision import transforms
 from tqdm import tqdm
 import json
 import time
+from tiling.tile_dataset import TilesDataset
 
 from my_model import MyModel
 from aws_utils.s3_sagemaker_utils import S3SageMakerUtils
@@ -64,21 +66,30 @@ def load_model(checkpoint_dir: str, my_model: MyModel, distributed: bool = False
     return epoch_number
 
 
-def initialize_data(train_dir: str, val_dir, filtration, filtration_cache, label_encoder, distributed=False, val=False):
+def initialize_data(train_dir: str, val_dir, filtration, filtration_cache, label_encoder, distributed=False, val=False, tiled=False, augmentation=False):
     dataset, data_loader = {}, {}
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation((0, 359))
+    ])
     dataset['train'] = Dataset(data_dir=train_dir,
                                labels=LabelManager(
                                    train_dir,
                                    label_postprocessor=label_encoder),
                                filtration=filtration,
-                               filtration_cache=filtration_cache)
+                               filtration_cache=filtration_cache,
+                               augmentation=transform if augmentation else None)
     if val:
         dataset['val'] = Dataset(data_dir=val_dir,
                                  labels=LabelManager(
                                      val_dir, label_postprocessor=label_encoder),
                                  filtration=filtration,
                                  filtration_cache=filtration_cache)
-
+                                 
+    if tiled:
+        dataset['train'] = TilesDataset(dataset['train'], '/opt/ml/scoring_data.json')
     train_sampler, val_sampler = None, None
     if distributed:
         train_sampler = DistributedSampler(
@@ -163,6 +174,12 @@ def main():
 
     def label_encoder(x):
         return labels[os.path.basename(x)]
+
+    try:
+        session.download_data(
+            '/opt/ml/', 'digpath-tilescore', 'digpath-data-new/scoring_data.json')
+    except:
+        print('No tiles')
 
     dataset, data_loader = initialize_data(train_dir, test_dir, filtration, filtration_cache, label_encoder,
                                            distributed=False, val=False)
